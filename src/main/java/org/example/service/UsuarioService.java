066af9c6f18b427bbf52067fbaf916210149cfb8
amelia <ameliamgalvao@gmail.com>
2023-12-05 23:12:07 -0300

    service

--- src/main/java/org/example/service/UsuarioService.java
+++ src/main/java/org/example/service/UsuarioService.java
@@ -4,30 +4,41 @@ import org.example.dto.UsuarioDTOInput;
 import org.example.dto.UsuarioDTOOutput;
 import org.example.model.Usuario;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 
 import java.util.List;
 
 public class UsuarioService {
     private List<Usuario> listaUsuarios;
+    private final ModelMapper modelMapper = new ModelMapper();
 
-    public List<UsuarioDTOOutput> listarUsuarios(){
-        modelMapper
-        return null;
+    public List<UsuarioDTOOutput> listarUsuarios() {
+        return modelMapper.map(listaUsuarios, new TypeToken<List<UsuarioDTOOutput>>() {}.getType());
     }
 
     public UsuarioDTOOutput searchUsuario(int id){
         //usar listaUsuarios, Usuario, UsuarioDTOOutput, ModelMapper.
+        //todo
         return null;
     }
 
-    public void addNovoUsuario(UsuarioDTOInput usuario){
+    public void addNovoUsuario(UsuarioDTOInput usuarioDTOInput){
+        Usuario usuario = modelMapper.map(usuarioDTOInput, Usuario.class);
+        listaUsuarios.add(usuario);
     }
 
-    public void changeUsuario(UsuarioDTOInput usuario){
-        //usar ModelMapper e listaUsuarios
+    public void changeUsuario(UsuarioDTOInput usuarioDTOInput){
+        Usuario usuario = modelMapper.map(usuarioDTOInput, Usuario.class);
+        //todo
     }
 
     public void deleteUsuario(int id){
-     //usar listaUsuarios e Usuario
+        for (int i = 0; i < listaUsuarios.size(); i++){
+            Usuario usuario = listaUsuarios.get(i);
+            if (usuario.getId() == id){
+                listaUsuarios.remove(i);
+                return;
+            }
+        }
     }
 }
